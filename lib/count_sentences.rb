341fc1980c61521636e2158bb36da2c2d28ef8e7
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-07-04 04:57:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+  key_array = []
+  count_sentences.each do |sentence, punctuation|
+    data.each do |sent1, sent2|
+      sent2.each do |value|
+        if value == ""
+        key_array << count_sentences.keys
+        end
+      end
+    end
+  end
+  key_array.flatten
   end
 end
+
