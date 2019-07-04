7ee56fa691497f4e5762bcfc3895367d9714add4
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-07-04 05:04:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).count.delete_if {|w|w.size > 2}
+    self.split(/\.|\?|\!/).count.delete_if {|w|w.size > 2}.size
     binding.pry
   end
 end
